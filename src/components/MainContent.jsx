026fd11f6b3f29c517f9787b61027f4652b21c85
Rocio-Infante <shadesofroseblog@gmail.com>
2021-02-18 19:46:33 -0800

    Displays main content for homepage depending on which category is clicked

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,42 @@
+import React from 'React';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import ArticleCard from './ArticleCard.jsx';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+const MainContent = ({ open, articleResults, articleCategories }) => {
+  const classes = useStyles();
+
+  return (
+    <main
+      className={clsx(classes.content, {
+        [classes.contentShift]: open,
+      })}
+    >
+      <div className={classes.drawerHeader} />
+      <ArticleCard articleResults={articleResults} articleCategories={articleCategories}/>
+    </main>
+  )
+};
+
+export default MainContent;
